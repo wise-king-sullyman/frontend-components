20e44fc8f03b130cd273e43e082a4f776a562dfb
Austin Sullivan <ausulliv@redhat.com>
2024-08-21 15:25:24 -0400

    color updates

--- packages/pdf-generator/src/utils/theme/themes/dark/multi-color-ordered-theme.js
+++ packages/pdf-generator/src/utils/theme/themes/dark/multi-color-ordered-theme.js
@@ -9,16 +9,16 @@ import chart_color_green_200 from '@patternfly/react-tokens/dist/js/chart_color_
 import chart_color_green_300 from '@patternfly/react-tokens/dist/js/chart_color_green_300';
 import chart_color_green_400 from '@patternfly/react-tokens/dist/js/chart_color_green_400';
 import chart_color_green_500 from '@patternfly/react-tokens/dist/js/chart_color_green_500';
-import chart_color_cyan_100 from '@patternfly/react-tokens/dist/js/chart_color_cyan_100';
-import chart_color_cyan_200 from '@patternfly/react-tokens/dist/js/chart_color_cyan_200';
-import chart_color_cyan_300 from '@patternfly/react-tokens/dist/js/chart_color_cyan_300';
-import chart_color_cyan_400 from '@patternfly/react-tokens/dist/js/chart_color_cyan_400';
-import chart_color_cyan_500 from '@patternfly/react-tokens/dist/js/chart_color_cyan_500';
-import chart_color_gold_100 from '@patternfly/react-tokens/dist/js/chart_color_gold_100';
-import chart_color_gold_200 from '@patternfly/react-tokens/dist/js/chart_color_gold_200';
-import chart_color_gold_300 from '@patternfly/react-tokens/dist/js/chart_color_gold_300';
-import chart_color_gold_400 from '@patternfly/react-tokens/dist/js/chart_color_gold_400';
-import chart_color_gold_500 from '@patternfly/react-tokens/dist/js/chart_color_gold_500';
+import chart_color_teal_100 from '@patternfly/react-tokens/dist/js/chart_color_teal_100';
+import chart_color_teal_200 from '@patternfly/react-tokens/dist/js/chart_color_teal_200';
+import chart_color_teal_300 from '@patternfly/react-tokens/dist/js/chart_color_teal_300';
+import chart_color_teal_400 from '@patternfly/react-tokens/dist/js/chart_color_teal_400';
+import chart_color_teal_500 from '@patternfly/react-tokens/dist/js/chart_color_teal_500';
+import chart_color_yellow_100 from '@patternfly/react-tokens/dist/js/chart_color_yellow_100';
+import chart_color_yellow_200 from '@patternfly/react-tokens/dist/js/chart_color_yellow_200';
+import chart_color_yellow_300 from '@patternfly/react-tokens/dist/js/chart_color_yellow_300';
+import chart_color_yellow_400 from '@patternfly/react-tokens/dist/js/chart_color_yellow_400';
+import chart_color_yellow_500 from '@patternfly/react-tokens/dist/js/chart_color_yellow_500';
 import chart_color_orange_100 from '@patternfly/react-tokens/dist/js/chart_color_orange_100';
 import chart_color_orange_200 from '@patternfly/react-tokens/dist/js/chart_color_orange_200';
 import chart_color_orange_300 from '@patternfly/react-tokens/dist/js/chart_color_orange_300';
@@ -31,28 +31,28 @@ import { ColorTheme } from '../color-theme';
 const COLOR_SCALE = [
   chart_color_blue_300.value,
   chart_color_green_300.value,
-  chart_color_cyan_300.value,
-  chart_color_gold_300.value,
+  chart_color_teal_300.value,
+  chart_color_yellow_300.value,
   chart_color_orange_300.value,
   chart_color_blue_100.value,
   chart_color_green_500.value,
-  chart_color_cyan_100.value,
-  chart_color_gold_100.value,
+  chart_color_teal_100.value,
+  chart_color_yellow_100.value,
   chart_color_orange_500.value,
   chart_color_blue_500.value,
   chart_color_green_100.value,
-  chart_color_cyan_500.value,
-  chart_color_gold_500.value,
+  chart_color_teal_500.value,
+  chart_color_yellow_500.value,
   chart_color_orange_100.value,
   chart_color_blue_200.value,
   chart_color_green_400.value,
-  chart_color_cyan_200.value,
-  chart_color_gold_200.value,
+  chart_color_teal_200.value,
+  chart_color_yellow_200.value,
   chart_color_orange_400.value,
   chart_color_blue_400.value,
   chart_color_green_200.value,
-  chart_color_cyan_400.value,
-  chart_color_gold_400.value,
+  chart_color_teal_400.value,
+  chart_color_yellow_400.value,
   chart_color_orange_200.value,
 ];
 
